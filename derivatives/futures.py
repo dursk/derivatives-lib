33dbe74c24ae0b1b572affdfb6eff95a7629beb7
Matthew Madurski <matt@matthewmadurski.com>
2014-11-23 11:09:30 -0500

    condense futures file

--- derivatives/futures.py
+++ derivatives/futures.py
@@ -6,14 +6,12 @@ from lib import formulas
 def get_fixed_storage_cost(storage_cost, risk_free_rate, term):
     return storage_cost * math.exp(-risk_free_rate * term)
 
-def get_commodities_price(spot_price, risk_free_rate, term, storage_cost=0,
-                          convienence_yield=0, fixed_storage_cost=False):
-    if fixed_storage_cost:
-        return (spot_price + get_fixed_storage_cost(
-            storage_cost, risk_free_rate, term
-        )) * math.exp(risk_free_rate * term)
-    return spot_price * math.exp((risk_free_rate + storage_cost) * term)
+def get_price(spot_price, risk_free_rate, term, known_income=0, known_yield=0):
+    return (spot_price - known_income) * math.exp(
+        (risk_free_rate - known_yield) * term
+    )
 
-def get_futures_price(spot_price, risk_free_rate, term, cost_of_carry,
-                      convenience_yield=0):
-    return spot_price * math.exp((cost_of_carry - convenience_yield) * term)
+def get_value(spot_price, delivery_price, risk_free_rate, term,
+              known_income=0, known_yield=0):
+    return (spot_price * math.exp(-known_yield * term)) - known_income - \
+        (delivery_price * math.exp(-risk_free_rate * term))
