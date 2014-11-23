246c6dea27f9623f96d3ea90d449e5938e8263df
Matthew Madurski <matt@matthewmadurski.com>
2014-11-23 09:34:55 -0500

    Futures on commodities pricing

--- derivatives/futures.py
+++ derivatives/futures.py
@@ -1,6 +1,15 @@
+import math
 from decimal import Decimal
 
 from lib import formulas
 
+def get_fixed_storage_cost(storage_cost, risk_free_rate, term):
+    return storage_cost * math.exp(-risk_free_rate * term)
 
-
+def get_commodities_price(spot_price, risk_free_rate, term, storage_cost=0,
+                          convienence_yield=0, fixed_storage_cost=False):
+    if fixed_storage_cost:
+        return (spot_price + get_fixed_storage_cost(
+            storage_cost, risk_free_rate, term
+        )) * math.exp(risk_free_rate * term)
+    return spot_price * math.exp((risk_free_rate + storage_cost) * term)
