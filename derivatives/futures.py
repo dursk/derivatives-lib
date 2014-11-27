9515ebf50989e5807ef5993be127c16dc14e9c1e
Matthew Madurski <matt@matthewmadurski.com>
2014-11-27 14:16:36 -0500

    get oil futures vs 6month bills

--- derivatives/futures.py
+++ derivatives/futures.py
@@ -22,8 +22,18 @@ def get_value(spot_price, delivery_price, risk_free_rate, term,
     return (spot_price * math.exp(-known_yield * term)) - known_income - \
         (delivery_price * math.exp(-risk_free_rate * term))
 
-def get_oil_vs_6month_rate():
-    for year in xrange(1983, 2011):
+def get_oil_vs_6month_rate(start, end):
+    oil_data = None
+    formatted_start = '{}-01-01'.format(start)
+    formatted_end = '{}-12-31'.format(end)
+    for year in xrange(start, end+1):
         for month in MONTHS:
             oil_contract = 'CME/B{}{}'.format(month, year)
             data = quandl.get(oil_contract)
+            oil_data = pd.concat([oil_data, data])
+    interest_data = quandl.get(
+        'FRED/DTB6',
+        start=formatted_start,
+        end=formatted_end
+    )
+    return oil_data, interest_data
--- derivatives/lib/quandl.py
+++ derivatives/lib/quandl.py
@@ -4,10 +4,22 @@ import os
 import requests
 import pandas as pd
 
+from auth import AUTH_TOKEN
+
+
 QUANDL_BASE_URL = 'http://quandl.com/api/v1/datasets'
 
-def get(contract):
-    r = requests.get('{}/{}.csv'.format(QUANDL_BASE_URL, contract))
+
+def get(contract, start=None, end=None):
+    url = '{}/{}.csv?auth_token={}'.format(
+        QUANDL_BASE_URL, contract, AUTH_TOKEN
+    )
+    if start:
+        url = '{}&trim_start={}'.format(url, start)
+    if end:
+        url = '{}&trim_end={}'.format(url, end)
+    print url
+    r = requests.get(url)
     with open('test.csv', 'wb') as csvfile:
         csvfile.write(r.text)
     data = pd.read_csv('test.csv')
