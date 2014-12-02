e75e069848f82a60a52d94762122b74b696cdde7
Matthew Madurski <matt@matthewmadurski.com>
2014-12-01 20:28:18 -0500

    better accuracy for start/end date in futures

--- derivatives/futures.py
+++ derivatives/futures.py
@@ -1,6 +1,7 @@
 import math
 from decimal import Decimal
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 import requests
 import numpy as np
@@ -10,7 +11,12 @@ from lib import formulas, quandl, helpers
 from data import contracts
 
 MONTHS = ['F', 'G', 'H', 'J', 'K', 'M', 'N', 'Q', 'U', 'V', 'X', 'Z']
-QUARTERS = ['H', 'M', 'U', 'Z']
+QUARTERS = {
+    'H': '03-31',
+    'M': '06-30',
+    'U': '09-30',
+    'Z': '12-31'
+}
 
 def get_fixed_storage_cost(storage_cost, risk_free_rate, term):
     return storage_cost * math.exp(-risk_free_rate * term)
@@ -38,9 +44,13 @@ def get_quarterly_diffs(contract, diff_length, start, end):
     ending_index = contracts.QUARTERLY.index(end)
     all_data = pd.Series()
     for i in xrange(starting_index, ending_index + 1):
+        current = contracts.QUARTERLY[i]
+        end_date = '{}-{}'.format(current[1:], QUARTERS[current[0]])
+        start_date = datetime.strptime(end_date, '%Y-%m-%d')
+        start_date = start_date - relativedelta(months=6)
         data = _get_data(
             contract + contracts.QUARTERLY[i],
-            start=None, end=None, collapse=collapse
+            start=start_date, end=end_date, collapse=collapse
         )
         all_data[data.index[-1]] = data[-1] - data[0]
     return all_data
